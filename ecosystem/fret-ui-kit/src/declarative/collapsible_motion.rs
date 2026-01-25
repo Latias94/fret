2b31a4215ff14736c1f039fd6c6ab668a9f6545e
Latias94 <superfrankie621@gmail.com>
2026-01-25 17:53:21 +0800

    refactor(ui-kit): prefer Px in layout refinements

--- ecosystem/fret-ui-kit/src/declarative/collapsible_motion.rs
+++ ecosystem/fret-ui-kit/src/declarative/collapsible_motion.rs
@@ -11,7 +11,7 @@ use fret_ui::elements::GlobalElementId;
 use fret_ui::{ElementContext, UiHost};
 
 use crate::headless::transition::TransitionOutput;
-use crate::{LayoutRefinement, MetricRef, Space};
+use crate::{LayoutRefinement, Space};
 
 /// Output describing how to render a collapsible-style measured-height wrapper for the current
 /// element root.
@@ -176,7 +176,7 @@ pub fn collapsible_height_wrapper_refinement(
     let mut wrapper = LayoutRefinement::default()
         .w_full()
         .min_w_0()
-        .min_h(MetricRef::Px(Px(0.0)))
+        .min_h(Px(0.0))
         .overflow_hidden();
     if wants_height_animation {
         wrapper = wrapper.h_px(Px(measured_height.0 * progress));
