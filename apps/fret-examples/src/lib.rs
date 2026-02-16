44f3922c0a2dc8a14775cda794d207d05d67d591
Latias94 <superfrankie621@gmail.com>
2026-02-16 10:28:27 +0800

    feat(interop): add embedded viewport demo

--- apps/fret-examples/src/lib.rs
+++ apps/fret-examples/src/lib.rs
@@ -47,6 +47,8 @@ pub mod echarts_demo;
 pub mod echarts_multi_grid_demo;
 #[cfg(not(target_arch = "wasm32"))]
 pub mod effects_demo;
+#[cfg(not(target_arch = "wasm32"))]
+pub mod embedded_viewport_demo;
 pub mod emoji_conformance_demo;
 pub mod error_bars_demo;
 #[cfg(not(target_arch = "wasm32"))]
