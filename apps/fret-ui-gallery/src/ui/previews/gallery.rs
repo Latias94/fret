d3dce5acc50f7bd124f2f647df25e19dde1bd9ba
Latias94 <superfrankie621@gmail.com>
2026-03-02 21:16:54 +0800

    chore(ui-gallery): prune legacy gallery preview modules

--- apps/fret-ui-gallery/src/ui/previews/gallery.rs
+++ apps/fret-ui-gallery/src/ui/previews/gallery.rs
@@ -1,11 +1,5 @@
-mod atoms;
 mod data;
-mod forms;
 mod overlays;
-mod torture;
 
-pub(in crate::ui) use atoms::*;
 pub(in crate::ui) use data::*;
-pub(in crate::ui) use forms::*;
 pub(in crate::ui) use overlays::*;
-pub(in crate::ui) use torture::*;
