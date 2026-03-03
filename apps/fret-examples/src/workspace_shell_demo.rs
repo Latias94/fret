a7310b5542f3013021bf670f66cfecb7acf30a3b
Latias94 <superfrankie621@gmail.com>
2026-03-03 12:24:17 +0800

    fix(workspace-shell-demo): widen default window

--- apps/fret-examples/src/workspace_shell_demo.rs
+++ apps/fret-examples/src/workspace_shell_demo.rs
@@ -1186,7 +1186,7 @@ pub fn run() -> anyhow::Result<()> {
 
     let config = WinitRunnerConfig {
         main_window_title: "fret-demo workspace_shell_demo".to_string(),
-        main_window_size: fret_launch::WindowLogicalSize::new(1080.0, 720.0),
+        main_window_size: fret_launch::WindowLogicalSize::new(1280.0, 720.0),
         ..Default::default()
     };
 
