3d23df4b5a2c49678d1e72982fb51181c886be19
Latias94 <superfrankie621@gmail.com>
2026-05-14 08:23:54 +0800

    refactor(imui): split shortcut interaction tests

---
+++ ecosystem/fret-imui/src/tests/interaction_shortcuts/mod.rs
@@ -0,0 +1,10 @@
+use super::*;
+
+use fret_runtime::{
+    CommandId, CommandMeta, DefaultKeybinding, PlatformFilter, WindowCommandEnabledService,
+};
+
+mod button_shortcuts;
+mod command_metadata;
+mod disclosure_tree;
+mod selectable_shortcuts;
