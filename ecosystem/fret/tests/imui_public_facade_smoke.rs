d0d319358e7412492b33f3440a3db06098b176de
Latias94 <superfrankie621@gmail.com>
2026-06-11 08:43:29 +0800

    test(fret): guard imui editor theme facade

--- ecosystem/fret/tests/imui_public_facade_smoke.rs
+++ ecosystem/fret/tests/imui_public_facade_smoke.rs
@@ -2,18 +2,32 @@
 
 use std::sync::Arc;
 
-use fret::component::prelude::{Px, UiHost};
+use fret::component::prelude::{Model, Px, UiHost};
 use fret::imui::{
     editor::{
         self,
         composites::{PropertyRow, PropertyRowOptions},
+        controls::{EditorThemePresetPicker, EditorThemePresetPickerOptions},
+        theme::EditorThemePresetV1,
     },
     kit::ListBoxOptions,
     prelude::UiWriter,
 };
 
 #[allow(dead_code)]
-fn root_imui_editor_composites_compile<H: UiHost + 'static>(ui: &mut impl UiWriter<H>) {
+fn root_imui_editor_composites_compile<H: UiHost + 'static>(
+    ui: &mut impl UiWriter<H>,
+    theme_preset_model: &Model<EditorThemePresetV1>,
+) {
+    editor::editor_theme_preset_picker(
+        ui,
+        EditorThemePresetPicker::new(theme_preset_model.clone()).options(
+            EditorThemePresetPickerOptions::new()
+                .test_id("tests.fret.imui.editor_theme")
+                .item_test_id_prefix("tests.fret.imui.editor_theme.item"),
+        ),
+    );
+
     editor::property_row(
         ui,
         PropertyRow::new().options(PropertyRowOptions {
@@ -28,6 +42,16 @@ fn root_imui_editor_composites_compile<H: UiHost + 'static>(ui: &mut impl UiWrit
 
 #[test]
 fn root_imui_facade_exposes_editor_composites_and_kit_sugar() {
+    assert_eq!(
+        EditorThemePresetV1::from_key("imgui-like-dense"),
+        Some(EditorThemePresetV1::ImguiLikeDense)
+    );
+
+    let _ = EditorThemePresetPickerOptions::new()
+        .disabled()
+        .without_label()
+        .test_id("tests.fret.imui.editor_theme")
+        .item_test_id_prefix("tests.fret.imui.editor_theme.item");
     let _ = PropertyRowOptions::default();
     let _ = ListBoxOptions::new()
         .width(Px(180.0))
